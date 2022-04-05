cb3b31c5bf530faf069997ce7c4f54e4a8cec7ce
emicif <emanuela.cifolelli@gmail.com>
2022-04-05 15:10:29 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Title msg="La mia prima app con VUE CLI!"/>
+    <Hero msg="JUMBOTRON della mia prima app con VUE CLI"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Title from './components/Title.vue'
+import Hero from './components/Hero.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Title,
+    Hero
   }
 }
 </script>
---
+++ src/components/Hero.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="jumbotron">
+      <p class="text-jumbotron">{{msg}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.jumbotron {
+  margin: 40px auto;
+  background-color: aquamarine;
+  height: 500px;
+}
+
+.text-jumbotron {
+  color: red;
+  font-size: 30px;
+  line-height: 500px;
+  vertical-align: middle;
+}
+</style>
